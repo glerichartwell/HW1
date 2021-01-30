a62def70679103a6794baf91f6b2a8c65951f3ba
Razgriz117 <martin.ds@knights.ucf.edu>
2021-01-30 00:34:43 -0500

    Struct Array Edit
    New implementation using struct instruction array.

--- vm.c
+++ vm.c
@@ -1,7 +1,8 @@
 // Glenn Hartwell
 // Martin de Salterain
-// COP3402 Spring 21
+// COP3402, Spring 2021
 // HW1 Virtual Machine
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -12,7 +13,7 @@
 typedef struct instruction
 {
 	int opcode;
-	char op[4]; // NOT SURE WHY THIS IS NEEDED IF YOU HAVE ANY IDEAS LET ME KNOW
+	char op[4]; // For storing instruction abbreviation
 	int l;
 	int m;
 } instruction;
@@ -20,18 +21,18 @@ typedef struct instruction
 int main(int argc, char *argv[])
 {
 	int *stack = calloc(MAX_STACK_HEIGHT, sizeof(int));
-	int **text = calloc(MAX_CODE_LENGTH, sizeof(int*));
+	instruction *text = calloc(MAX_CODE_LENGTH, sizeof(instruction));
 
-	
 	FILE *fp;
-	instruction IR = NULL;
-	
-	int SP = -1;
-	int BP = 0;
-	int PC = 0;
+	instruction *ir = NULL;
+
+	int sp = -1;
+	int bp = 0;
+	int pc = 0;
 	int halt = 0;
 	int i, j;
-	
+	int scan_buffer;
+
 	// Check proper argument count
 	if (argc != 2)
 	{
@@ -39,59 +40,84 @@ int main(int argc, char *argv[])
 		return 0;
 	}
 
-	// text is an array of arrays that hold the integers for each instruction
-	// IF YOU THINK OF A BETTER WAY TO STORE THE INSTRUCTIONS DO IT!
-	// Initialize text arrays
-	for (i = 0; i < MAX_CODE_LENGTH; i++)
+	// Load instructions into array from file. (Check for EOF).
+	fp = fopen(argv[1], "r");
+	for(i = 0, fscanf(fp, "%d", &scan_buffer); i < MAX_CODE_LENGTH, scan_buffer!= EOF; i++)
 	{
-		text[i] = calloc(INSTRUCTION_LENGTH, sizeof(int));
+				text[i].opcode = scan_buffer;
+				fscanf(fp, "%d", &(text[i].l));
+				fscanf(fp, "%d", &(text[i].m));
 	}
 
-	// Load instructions into array from file
-	fp = fopen(argv[1], "r");
-	for(i = 0; i < MAX_CODE_LENGTH; i++)
-	{
-		for (j = 0; j < INSTRUCTION_LENGTH; j++)
-		{
-			fscanf(fp, "%d", &text[i][j]); 
-		}
-	}
-	// Print to make sure it loaded the arrays properly
-	// Can be deleted later
-	/*
-	for(i = 0; i < MAX_CODE_LENGTH; i++)
-	{
-		for (j = 0; j < INSTRUCTION_LENGTH; j++)
-		{
-			printf("%d ", text[i][j]);
-		}
-		printf("\n");
-	}
-	*/
 	fclose(fp);
 
+	ir = calloc(1, sizeof(instruction));
 
 	while (!halt)
 	{
 		// Fetch
-		IR.opcode = text[PC][0]; // opcode is first num in instruction
-		IR.l = text[PC][1]; 	 // l is second num in instruction
-		IR.m = text[PC][2];		 // m is third num in instruction
-		PC += 1;
+		ir->opcode = text[pc].opcode;
+		ir->l = text[pc].l;
+		ir->m = text[pc].m;
+		pc += 1;
+
+		// Remember to populate ir->op[] array with string abbreviating operation.
 
 		// Execute
+		switch(ir->opcode)
+		{
+			case 1: //LIT: Pushes a literal value "m" onto the stack.
+				sp++;
+				stack[sp] = ir->m;
+				strcpy("LIT", ir->op);
+				break;
 
+			case 2: //OPR
+				if ()
+				{
+
+				}
+				break;
+
+			// LOD: Pushes an element onto the stack at "l" lexicographical levels down
+			// plus some offset "m."
+			case 3:
+				sp++;
+				stack[sp] = stack[base(stack, ir->l, bp) + ir.m];
+				strcpy("LOD", ir->op);
+				break;
+
+			case 4: //STO
+				break;
+
+			case 5: //CAL
+				break;
+
+			case 6: //INC
+				break;
+
+			case 7: //JMP
+				break;
+
+			case 8: //JPC
+				break;
+
+			case 9: //SYS
+				break;
+		}
 
 	}
-	
+
 
 
 	return 0;
 }
 
-int base(int stack[], int level, int BP)
+// Returns the index of the stack indicating the base of the activation record found at some
+// given lexicographical level.
+int base(int stack[], int level, int bp)
 {
-	int base = BP;
+	int base = bp;
 	int counter = level;
 	while (counter > 0)
 	{
