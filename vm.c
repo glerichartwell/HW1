47e23a9998821cc580847f3a245f5b7caed125b2
Razgriz117 <martin.ds@knights.ucf.edu>
2021-01-30 20:08:38 -0500

    Debugged Segfault
    It's been so long since I worked with strings in C that I mixed up the parameter order for strcpy and the program was trying to access the char array holding a string literal, which is stored in read-only memory. lol

--- vm.c
+++ vm.c
@@ -5,6 +5,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define MAX_STACK_HEIGHT 50
 #define MAX_CODE_LENGTH 100
@@ -18,233 +19,6 @@ typedef struct instruction
 	int m;
 } instruction;
 
-int main(int argc, char *argv[])
-{
-	int *stack = calloc(MAX_STACK_HEIGHT, sizeof(int));
-	instruction *text = calloc(MAX_CODE_LENGTH, sizeof(instruction));
-
-	FILE *fp;
-	instruction *ir = NULL;
-
-	int sp = -1;
-	int bp = 0;
-	int pc = 0;
-	int halt = 1;
-	int i, j;
-	int scan_buffer;
-
-	// Check proper argument count
-	if (argc != 2)
-	{
-		printf("The format for compiling is: ./a.out your_file_name.txt\n");
-		return 0;
-	}
-
-	// Load instructions into array from file. (Check for EOF).
-	fp = fopen(argv[1], "r");
-	for(i = 0, fscanf(fp, "%d", &scan_buffer); i < MAX_CODE_LENGTH, scan_buffer!= EOF; i++)
-	{
-				text[i].opcode = scan_buffer;
-				fscanf(fp, "%d", &(text[i].l));
-				fscanf(fp, "%d", &(text[i].m));
-	}
-
-	fclose(fp);
-
-	ir = calloc(1, sizeof(instruction));
-
-	while (halt)
-	{
-		// Fetch
-		ir->opcode = text[pc].opcode;
-		ir->l = text[pc].l;
-		ir->m = text[pc].m;
-		pc += 1;
-
-		// Execute
-		switch(ir->opcode)
-		{
-			//LIT: Pushes a literal value "m" onto the stack.
-			case 1:
-				sp++;
-				stack[sp] = ir->m;
-				strcpy("LIT", ir->op);
-				break;
-
-			// OPR: Performs some operation based on the value of m.
-			case 2:
-				switch (ir->m)
-				{
-					// RTN: Restore the environment of the caller.
-					case 0:
-						stack[bp - 1] = stack[sp]; // Retain value on top of the stack.
-						sp = bp - 1;
-						bp = stack[sp + 2]; // Base of restored AR = DL of current AR.
-						pc = stack[sp + 3]; // Restore program counter to return address.
-						strcpy("RTN", ir->op);
-						break;
-
-					case 1: // NEG
-						stack[sp] = -1 * stack[sp];
-						strcpy("NEG", ir->op);
-						break;
-
-					case 2: // ADD
-						sp--;
-						stack[sp] = stack[sp] + stack[sp + 1];
-						strcpy("ADD", ir->op);
-						break;
-
-					case 3: // SUB
-						sp--;
-						stack[sp] = stack[sp] - stack[sp + 1];
-						strcpy("SUB", ir->op);
-						break;
-
-					case 4: // MUL
-						 sp--;
-						stack[sp] = stack[sp] * stack[sp + 1];
-						strcpy("MUL", ir->op);
-						break;
-
-					case 5: // DIV
-						sp--;
-						stack[sp] = stack[sp] / stack[sp + 1];
-						strcpy("DIV", ir->op);
-						break;
-
-					case 6: // ODD
-						stack[sp] = stack[sp] mod 2;
-						strcpy("ODD", ir->op);
-						break;
-
-					case 7: // MOD
-						sp--;
-						stack[sp] = stack[sp] % stack[sp + 1];
-						strcpy("MOD", ir->op);
-						break;
-
-					case 8: // EQL (==)
-						sp--;
-						stack[sp] = (stack[sp] == stack[sp + 1]);
-						strcpy("EQL", ir->op);
-						break;
-
-					case 9:	// NEQ (!=)
-						sp--;
-						stack[sp] = (stack[sp] != stack[sp + 1]);
-						strcpy("NEQ", ir->op);
-						break;
-
-					case 10: // LSS (<)
-						sp--;
-						stack[sp] = (stack[sp] < stack[sp + 1]);
-						strcpy("LSS", ir->op);
-						break;
-
-					case 11: // LEQ (<=)
-						sp--;
-						stack[sp] = (stack[sp] <= stack[sp + 1]);
-						strcpy("LEQ", ir->op);
-						break;
-
-					case 12: // GTR (>)
-						sp--;
-						stack[sp] = (stack[sp] > stack[sp + 1]);
-						strcpy("GTR", ir->op);
-						break;
-
-					case 13: // GEQ (>=)
-						sp--;
-						stack[sp] = (stack[sp] >= stack[sp + 1]);
-						strcpy("GEQ", ir->op);
-						break;
-				}
-				break;
-
-			// LOD: Pushes an element at "l" lexicographical levels down plus some offset
-			// "m" onto the stack .
-			case 3:
-				sp++;
-				stack[sp] = stack[base(stack, ir->l, bp) + ir.m];
-				strcpy("LOD", ir->op);
-				break;
-
-			// STO: Stores the element from the top of the stack at some index "l"
-			// lexicographical levels down plus an offset "m".
-			case 4:
-				stack[base(stack, ir->l, bp) + ir->m] = stack[sp];
-				sp--;
-				strcpy("STO", ir->op);
-				break;
-
-			// CAL: Calls a fuction whose instruction is indexed in the text by "m."
-			case 5:
-				stack[sp + 1] = base(stack, ir->l, bp);           // static link (SL)
-				stack[sp + 2] = bp;                               // dynamic link (DL)
-				stack[sp + 3] = pc;                               // return address (RA)
-				stack[sp + 4] = stack[sp];                        // parameter (P)
-				bp = sp + 1;                                      // new base pointer (BP)
-
-				//Point program counter to instruction of the callee.
-				pc = ir->m;
-				strcpy("CAL", ir->op);
-				break;
-
-			// INC: Used to increment the stack pointer by some offset "m."
-			case 6:
-				sp = sp + ir->m;
-				strcpy("INC", ir->op);
-				break;
-
-			// JMP: Sets the next instruction to one at index "m."
-			case 7:
-				pc = ir->m;
-				strcpy("JMP", ir->op);
-				break;
-
-			// JPC: Executes a conditional jump based on the value at the top of the stack.
-			case 8:
-				if (stack[sp] == 0)
-				{
-					pc = ir->m;
-					sp--;
-				}
-
-				strcpy("JPC", ir->op);
-				break;
-
-			// SYS: Initiates a system call based on the value of m.
-			case 9:
-				strcpy("SYS", ir->op);
-				switch(ir->m)
-				{
-					case 1:
-						printf("%d", stack[sp]);
-						sp--;
-						break;
-
-					case 2: // Receive input from user.
-						sp++;
-						// NOTE: Initiate waiting loop here until user inputs value/
-						scanf("%d", stack[sp]);
-						break;
-
-					case 3: // Halt program
-						halt = 0;
-						break;
-				}
-			break;
-
-			default:
-				printf("opcode error: This instruction does not exist.\n");
-				break;
-		}
-	}
-
-	return 0;
-}
-
 // Returns the index of the stack indicating the base of the activation record found at some
 // given lexicographical level.
 int base(int stack[], int level, int bp)
@@ -257,4 +31,263 @@ int base(int stack[], int level, int bp)
 		counter--;
 	}
 	return base;
+}
+
+int main(int argc, char *argv[])
+{
+	int *stack = calloc(MAX_STACK_HEIGHT, sizeof(int));
+	int *ar_delimiter_stack = calloc(MAX_STACK_HEIGHT, sizeof(int));
+	instruction *text = calloc(MAX_CODE_LENGTH, sizeof(instruction));
+
+	FILE *fp;
+	instruction *ir = NULL;
+
+	int sp = -1;
+	int bp = 0;
+	int pc = 0;
+	int halt = 1;
+	int i, line_number;
+	int scan_buffer;
+
+	// Check proper argument count
+	if (argc != 2)
+	{
+		printf("The format for compiling is: ./a.out your_file_name.txt\n");
+		return 0;
+	}
+
+	// Load instructions into array from file. (Check for EOF).
+	if ((fp = fopen(argv[1], "r")) == NULL)
+	{
+		printf("Error: failed to open input file. Exiting...\n");
+		return 0;
+	}
+
+	for (i = 0; fscanf(fp, "%d", &scan_buffer) != EOF && i < MAX_CODE_LENGTH; i++)
+	{
+				text[i].opcode = scan_buffer;
+				fscanf(fp, "%d", &(text[i].l));
+				fscanf(fp, "%d", &(text[i].m));
+	}
+
+	fclose(fp);
+
+	ir = calloc(1, sizeof(instruction));
+
+	/*
+	printf("IR: %d %d %d\n", ir->opcode, ir->l, ir->m);
+	printf("TEXT: %d %d %d\n", text[pc].opcode, text[pc].l, text[pc].m);
+	printf("IR->OP: %s\n", ir->op);
+	*/
+
+	// Print headder for stack trace.
+	printf("                  PC   BP   SP   stack\n");
+	printf("Initial values:   %d   %d   %d   \n", pc, bp, sp);
+
+	while (halt)
+	{
+		// Fetch
+		ir->opcode = text[pc].opcode;
+		ir->l = text[pc].l;
+		ir->m = text[pc].m;
+		line_number = pc;
+		pc++;
+
+		// Execute
+		switch(ir->opcode)
+		{
+			//LIT: Pushes a literal value "m" onto the stack.
+			case 1:
+				sp++;
+				stack[sp] = ir->m;
+				strcpy(ir->op, "LIT");
+				break;
+
+			// OPR: Performs some operation based on the value of m.
+			case 2:
+				switch (ir->m)
+				{
+					// RTN: Restore the environment of the caller.
+					case 0:
+						ar_delimiter_stack[bp - 1] = 0; // Facilitates print format
+						stack[bp - 1] = stack[sp]; // Retain value on top of the stack.
+						sp = bp - 1;
+						bp = stack[sp + 2]; // Base of restored AR = DL of current AR.
+						pc = stack[sp + 3]; // Restore program counter to return address.
+						strcpy(ir->op, "RTN");
+						break;
+
+					case 1: // NEG
+						stack[sp] = -1 * stack[sp];
+						strcpy(ir->op, "NEG");
+						break;
+
+					case 2: // ADD
+						sp--;
+						stack[sp] = stack[sp] + stack[sp + 1];
+						strcpy(ir->op, "ADD");
+						break;
+
+					case 3: // SUB
+						sp--;
+						stack[sp] = stack[sp] - stack[sp + 1];
+						strcpy(ir->op, "SUB");
+						break;
+
+					case 4: // MUL
+						 sp--;
+						stack[sp] = stack[sp] * stack[sp + 1];
+						strcpy(ir->op, "MUL");
+						break;
+
+					case 5: // DIV
+						sp--;
+						stack[sp] = stack[sp] / stack[sp + 1];
+						strcpy(ir->op, "DIV");
+						break;
+
+					case 6: // ODD
+						stack[sp] = stack[sp] % 2;
+						strcpy(ir->op, "ODD");
+						break;
+
+					case 7: // MOD
+						sp--;
+						stack[sp] = stack[sp] % stack[sp + 1];
+						strcpy(ir->op, "MOD");
+						break;
+
+					case 8: // EQL (==)
+						sp--;
+						stack[sp] = (stack[sp] == stack[sp + 1]);
+						strcpy(ir->op, "EQL");
+						break;
+
+					case 9:	// NEQ (!=)
+						sp--;
+						stack[sp] = (stack[sp] != stack[sp + 1]);
+						strcpy(ir->op, "NEQ");
+						break;
+
+					case 10: // LSS (<)
+						sp--;
+						stack[sp] = (stack[sp] < stack[sp + 1]);
+						strcpy(ir->op, "LSS");
+						break;
+
+					case 11: // LEQ (<=)
+						sp--;
+						stack[sp] = (stack[sp] <= stack[sp + 1]);
+						strcpy(ir->op, "LEQ");
+						break;
+
+					case 12: // GTR (>)
+						sp--;
+						stack[sp] = (stack[sp] > stack[sp + 1]);
+						strcpy(ir->op, "GTR");
+						break;
+
+					case 13: // GEQ (>=)
+						sp--;
+						stack[sp] = (stack[sp] >= stack[sp + 1]);
+						strcpy(ir->op, "GEQ");
+						break;
+				}
+				break;
+
+			// LOD: Pushes an element at "l" lexicographical levels down plus some offset
+			// "m" onto the stack .
+			case 3:
+				sp++;
+				stack[sp] = stack[base(stack, ir->l, bp) + ir->m];
+				strcpy(ir->op, "LOD");
+				break;
+
+			// STO: Stores the element from the top of the stack at some index "l"
+			// lexicographical levels down plus an offset "m".
+			case 4:
+				stack[base(stack, ir->l, bp) + ir->m] = stack[sp];
+				sp--;
+				strcpy(ir->op, "STO");
+				break;
+
+			// CAL: Calls a fuction whose instruction is indexed in the text by "m."
+			case 5:
+				ar_delimiter_stack[sp] = 1;                       // Facilitates print format
+				stack[sp + 1] = base(stack, ir->l, bp);           // static link (SL)
+				stack[sp + 2] = bp;                               // dynamic link (DL)
+				stack[sp + 3] = pc;                               // return address (RA)
+				stack[sp + 4] = stack[sp];                        // parameter (P)
+				bp = sp + 1;                                      // new base pointer (BP)
+
+				//Point program counter to instruction of the callee.
+				pc = ir->m;
+				strcpy(ir->op, "CAL");
+				break;
+
+			// INC: Used to increment the stack pointer by some offset "m."
+			case 6:
+				sp = sp + ir->m;
+				strcpy(ir->op, "INC");
+				break;
+
+			// JMP: Sets the next instruction to one at index "m."
+			case 7:
+				pc = ir->m;
+				strcpy(ir->op, "JMP");
+				break;
+
+			// JPC: Executes a conditional jump based on the value at the top of the stack.
+			case 8:
+				if (stack[sp] == 0)
+				{
+					pc = ir->m;
+					sp--;
+				}
+
+				strcpy(ir->op, "JPC");
+				break;
+
+			// SYS: Initiates a system call based on the value of m.
+			case 9:
+				strcpy(ir->op, "SYS");
+				switch(ir->m)
+				{
+					case 1:
+						printf("Top of Stack Value: ");
+						printf("%d\n", stack[sp]);
+						sp--;
+						break;
+
+					case 2: // Receive input from user.
+						sp++;
+						printf("Please Enter an Integer: ");
+						scanf("%d", &stack[sp]);
+						printf("\n");
+						break;
+
+					case 3: // Halt program
+						halt = 0;
+						break;
+				}
+			break;
+
+			default:
+				printf("opcode error: This instruction does not exist.\n");
+				break;
+		}
+
+		// Print current state.
+		printf("%d %s  %d %d    %d    %d    %d", line_number, ir->op, ir->l, ir->m, pc, bp, sp);
+		printf("  ");
+		for(i = 0; i <= sp; i++)
+		{
+			printf(" %d", stack[i]);
+			if (ar_delimiter_stack[i] == 1)
+				printf(" |");
+		}
+		printf("\n");
+	}
+
+	return 0;
 }
